c88a83f4cdfba046eaf0829b9d31770825cfbfcc
S: Kiggins <jkiggins2@student.monroecc.edu>
2016-07-07 15:53:49 -0400

    i might clean this up eventually

--- lib/calibration.h
+++ lib/calibration.h
@@ -1,21 +1,24 @@
 #ifndef calibration
 #define calibration
-	#define MAX_SENSORS 8
+
+#include <Arduino.h>
 
 	typedef struct
 	{
 		int low, high;
 		float scale;
 		int pin;
+		int type;
 	} sensor;
 
-	class calibration
+	class sensor_array
 	{
 		private:
-			sensor sns [MAX_SENSORS];
-			int eepoff // EEPROM offset in bytes
+			sensor * sns;
+			int eeprom_origin;
+			static int eeprom_off;
 		public:
-			void calibration(int argEEPOFF);
+			sensor_array(int);
 			void push_cval(int val);
 			void save();
 			void load();
@@ -24,6 +27,20 @@
 			int read_sensor(int i);
 	};
 
+	sensor_array::sensor_array(int len)
+	{
+		sns = new sensor[len];
+	}
+
+	int sensor_array::read_sensor(int i)
+	{
+		return (analogRead(sns[i].pin) - sns[i].low)*sns[i].scale;
+	}
+
+	sensor& sensor_array::get_sensor(int i)
+	{
+		return &sns[i];
+	}
+
+
 #endif
-
-
